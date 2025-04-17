fcdd9445174ea43796bb39ca3528aa18668de136
JuanUribe7 <uribej030@gmail.com>
2025-04-17 01:45:59 -0500

    inicio trazabilidad

---
+++ src/main/java/com/example/plazoleta/ms_trazabilidad/MsTrazabilidadApplication.java
@@ -0,0 +1,13 @@
+package com.example.plazoleta.ms_trazabilidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTrazabilidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTrazabilidadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/plazoleta/ms_trazabilidad/MsTrazabilidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plazoleta.ms_trazabilidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTrazabilidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
